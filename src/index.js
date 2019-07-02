9e14bdd4f5514145c0119c3298c8dc936c89e54f
AnthonySBR <asbr1811@gmail.com>
2019-07-02 12:41:30 +0200

    Initial route

---
+++ src/index.js
@@ -0,0 +1,25 @@
+const express = require ('express');
+const app = express ();
+const morgan = require ('morgan');
+const path = require('path');
+
+// Settings
+app.set('port', process.env.PORT || 3000);
+
+// Middlewares
+app.use(morgan('dev'));
+app.use(express.json())
+
+
+// Routes
+app.use('/api/tasks', require('./routes/task-routes'));
+
+
+
+// Public Static files
+app.use(express.static(path.join(__dirname, 'public')))
+
+// Starting server
+app.listen(app.get('port'), () => {
+    console.log(`Server on port ${app.get('port')}`)
+})
